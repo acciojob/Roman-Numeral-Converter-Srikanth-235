6a82c0659e01f7493a839de3a4de3353eac49b22
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2025-06-21 10:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ function convertToRoman(num) {
       3:['L', 50], 
       4:['X', 10], 
       5:['V', 5], 
-      6:['I', 1]
+      6:['I', 1],
+	7:['CM',900],
+	8:['XC',90],
+	9:['IX',9],
+	10:['XL',40],
+	11:['CD',400]
+		
     };
-
+	let res='';
+   for(let i=0;i<Object.keys(obj).length;i++){
+	   const[romanno,value]=obj[i];
+	   //destructering
+	   while(num>=value){
+		   res+=romanno;
+		   num=num-value;
+	   }
+   }
   //your code here
 
 }
